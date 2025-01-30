4cc0cb513c3b1b6bb40740b56d55b294fd27146e
Suman01-tatva <tatva.pci93@outlook.com>
2025-01-30 17:26:30 +0530

    Update the code

--- Pages/js/navbar.js
+++ Pages/js/navbar.js
@@ -1,3 +1,7 @@
-document.querySelector("#toggle-btn")?.addEventListener("click", function () {
+document.querySelector(".toggle-btn")?.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("show");
+});
+
+document.querySelector("#close-btn")?.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.remove("show");
 });
